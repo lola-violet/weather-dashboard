546f78ff0d151190f3800d0aea79d83b61a1c3c3
Lola Applegate <laurenemilya@gmail.com>
2022-04-12 18:32:53 -0700

    updated readme, cleaned up html & js, added some styling elements

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # weather-dashboard
 UWBC HW#6: Server-Side APIs: Weather Dashboard
+<br>
+<br>
+
+# Description
+A web application that allows users to search for a city & see that location's current weather & 5-day forecast. 
+<br>
+<br>
+
+# Points of Interest
+## Search Bar
+Users can enter the name of any major city, instead of having to enter a city code or precise latitude & longitude. 
+I utilized two APIs & local storage in order to make this possible. The first API returns location data for any given location based on the city's name. The latitude & longitude are then stored locally, where they can be retrieved by the second API. The second API returns current weather data as well as a 5-day forecast.
+<br>
+
+## Current Weather
+When the user enters a city name and clicks the search button, current weather data for that location will appear in the large box. 
+In addition to pulling current weather data from the One Call API, I incorporated Moment.JS to convert the data's Unix Timestamp into a readable date.
+<br>
+
+## 5-Day Forecast
+Beneath the current weather box, the user will see 5 smaller boxes, each with a date & weather data for the next 5 days.
+I used the One Call API & Moment.JS to show each date & its respective data. 
+<br>
+<br>
+
+# Deployed Application
+
+<br>
+<br>
+
+# Usage
+
+<br>
+<br>
--- script.js
+++ script.js
@@ -21,7 +21,6 @@ var currWind = $('#current-wind');
 var currHum = $('#current-humidity');
 var uvIndex = $('#uv-index');
 
-var day1 = $('#day-1');
 var dateCardEl = $('.forecast');
 var date = $('.date');
 var temp = $('.temp');
@@ -77,12 +76,11 @@ function getWeather() {
         currHum.text("Humidity: " + data.current.humidity + "%");
         uvIndex.text("UV Index: " + data.current.uvi);
 
-     
+        // Display forecast weather data
         dateCardEl.each(function() {
             var cardID = $(this).attr("id");
             var foreUnix = moment.unix(data.daily[cardID].dt).format("MM/DD/YYYY");
             $(this).children().eq(0).text(foreUnix);
-
             $(this).children().eq(2).text("Temp: " + data.daily[cardID].temp.day + "° F");
             $(this).children().eq(3).text("Wind: " + data.daily[cardID].wind_speed + " MPH");
             $(this).children().eq(4).text("Humidity: " + data.daily[cardID].humidity + "%");
@@ -90,54 +88,10 @@ function getWeather() {
     })
 }
 
-// Fetch request for current weather data
-function getCurrentWeather() {
-    // Necessary variables for fetch request
-    var cityInput = searchInputEl.val();
-    var requestCurrentUrl = "http://api.openweathermap.org/data/2.5/weather?q=" +cityInput+ "&appid=" +apiKey+ "&units=imperial";
-    // Fetching data
-    fetch(requestCurrentUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    // Displaying data
-    .then(function (data) {
-        console.log(data);
-        cityName.text(data.name);
-        currTemp.text("Temp: " + data.main.temp + "° F");
-        currWind.text("Wind: " + data.wind.speed + " MPH");
-        currHum.text("Humidity: " + data.main.humidity + "%");
-    })
-}
-
-// Fetch request for 5-day forecast data
-function getForecast() {
-    // Necessary variables for fetch request
-    var cityInput = searchInputEl.val();
-    var requestForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" +cityInput+ "&appid=" +apiKey+ "&units=imperial";
-    // Fetching data
-    fetch(requestForecastUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    // Displaying data
-    .then(function (data) {
-        console.log(data);
-
-        date.text(data.list[3].dt_txt);
-        temp.text(data.list[3].main.temp);
-        wind.text(data.list[3].wind.speed);
-        hum.text(data.list[3].main.humidity);
-    })
-}
 
 // Click listeners for search button
-// To retreive geo coordinates
+// To retreive geo coordinates & weather data
 searchBtn.click(getCoordinates);
-// // To display current weather data
-// searchBtn.click(getCurrentWeather);
-// // To display 5-day forecast
-// searchBtn.click(getForecast);
 // // To save search inputs into loval file
 searchBtn.click(saveHistory);
 
@@ -158,22 +112,3 @@ function saveHistory(event) {
     
 }
 
-// Display search history below form (clickable)
-// prevSearch.each(function() {
-//     $(this).text(localStorage.getItem());
-// })
-
-// WEATHER DATA
-// City
-// Date
-// Temp
-// Wind
-// Humidity
-// UV Index (color-coded)
-
-// 5 DAY FORECAST CARDS
-// Date
-// Icon
-// Temp
-// Wind 
-// Humidity
